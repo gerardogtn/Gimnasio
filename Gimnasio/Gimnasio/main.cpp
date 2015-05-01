e39cf01130468fbbdcc8575aa299e8b58d622900
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-01 13:11:55 -0500

    Created c++ project

---
+++ Gimnasio/Gimnasio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gimnasio
+//
+//  Created by Gerardo Teruel on 5/1/15.
+//  Copyright (c) 2015 Gerardo Teruel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
