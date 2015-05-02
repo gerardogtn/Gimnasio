362ca7a076c45e769aee2cc67517150a275103ca
act2108 <acampostams@gmail.com>
2015-05-02 00:26:52 +0000

    Implemented menu w/ integer validation

--- Gimnasio/Gimnasio/main.cpp
+++ Gimnasio/Gimnasio/main.cpp
@@ -10,6 +10,8 @@
 
 #include "Gimnasio.h"
 
+int getInt(int);
+
 int main(int argc, const char * argv[]) {
     Gimnasio sportsWorld("Sports world");
     
@@ -36,5 +38,87 @@ int main(int argc, const char * argv[]) {
     
     sportsWorld.printMachines();
     sportsWorld.printUsers();
+    
+    // Menú de Opciones para el Usuario
+    int option=1;
+    while (option!=6)
+    {
+        std::cout <<"Bienvenido a Sports World \n";
+        std::cout <<"1. Registrar Nuevo Usuario \n";
+        std::cout <<"2. Registrar una Nueva Máquina \n";
+        std::cout <<"3. Mostrar la Cantidad de Usuarios y de Máquinas del Gimnasio \n";
+        std::cout <<"4. Mostrar las Máquinas Disponibles \n";
+        std::cout <<"5. Mostrar los Usuarios del Gimnasio\n";
+        std::cout <<"6. Salir \n";
+        std::cin >> option;
+        getInt(option);
+        switch (option)
+        {
+            case 1:
+            {
+                std::string name;
+                std::cout << "Ingresa el nombre del usuario a agregar"<<std::endl;
+                std::cin.ignore();
+                std::getline(std::cin,name);
+                std::string last;
+                std::cout << "Ingresa el apellido del usuario"<<std::endl;
+                std::cin.ignore();
+                std::getline(std::cin,last);
+                
+                sportsWorld.addUser(name,last);
+            }
+            break;
+            case 2:
+            {
+                std::string name;
+                std::cout << "Ingresa el nombre de la máquina a agregar"<<std::endl;
+                std::cin.ignore();
+                std::getline(std::cin,name);
+                std::string type;
+                std::cout << "Ingresa el tipo de máquina que es: pecho, espalda, brazo, pierna o abdomen"<<std::endl;
+                std::cin.ignore();
+                std::getline(std::cin,type);
+                
+                Maquina newMachine(name,type);
+                
+                sportsWorld.addMachine(newMachine);
+            }
+            break;
+            case 3:
+            {
+                std::cout << sportsWorld;
+            }
+            break;
+            case 4:
+            {
+                sportsWorld.printMachines();
+            }
+            break;
+            case 5:
+            {
+                sportsWorld.printUsers();
+            }
+            break;
+            case 0:
+            break;
+        }
+    }
+    
     return 0;
+}
+
+int getInt(int value)
+{
+    if ( std::cin && value > 0 && value <= 6)
+    {
+        return value;
+    }
+    else
+    {
+        std::cout << "Por favor ingrese un valor numérico válido" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cin >> value;
+        getInt(value);
+    }
 }
